11e17ef1b57d43b6bec40b2218d16e414453b8cf
Samir Hassen <Samir@sonofacoder.com>
2024-12-24 00:53:50 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+public class Solution {
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int MaxDepth(TreeNode root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        var leftMaxDepth = MaxDepth(root.left);
+        var rightMaxDepth = MaxDepth(root.right);
+
+        return Math.Max(leftMaxDepth, rightMaxDepth) + 1;
+    }
+}
